f82cbebf928e99b0307183b0e4e6365c2b3a1f35
Lcorinna <Gaida95@yandex.ru>
2024-11-30 00:13:55 +0300

    Организация стилей. Добавлены темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React, { Suspense } from 'react';
-import './index.scss';
+import React, { Suspense, useContext, useState } from 'react';
+import './styles/index.scss';
 import { Route, Routes, Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { ThemeContext, Theme } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+
 
 export const App = () => {
+	const {theme, toggleTheme} = useTheme()
+
 	return (
-		<div className='app'>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Change theme</button>
 			<Link to='/'>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Loading...</div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,22 @@
+import React, {useState, useMemo} from 'react'
+import { LOCAL_STORAGE_THEME_KEY, ThemeContext, Theme } from './ThemeContext'
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+const ThemeProvider: React.FC = ({children}) => {
+
+	const [theme, setTheme] = useState<Theme>(defaultTheme)
+	
+	const defaultProps = useMemo(() => ({
+		theme,
+		setTheme
+	}), [theme])
+
+	return (
+		<ThemeContext.Provider value={defaultProps}>
+			{children}
+		</ThemeContext.Provider>
+	)
+}
+
+export default ThemeProvider
