af13913a1a646eee8bdcefd15a111eed673f4c42
Lcorinna <Gaida95@yandex.ru>
2024-12-02 13:29:09 +0300

    Navbar. Шаблоны для разработки. Первый UI Kit компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes, Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
+import { NavBar } from 'widgets/NavBar';
 
 
 export const App = () => {
@@ -11,10 +12,9 @@ export const App = () => {
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<button onClick={toggleTheme}>Change theme</button>
-			<Link to='/'>Главная</Link>
-			<Link to={'/about'}>О сайте</Link>
+			<NavBar />
 			<AppRouter/>
+			<button onClick={toggleTheme}>Change theme</button>
 		</div>
 	)
 }
---
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -0,0 +1,23 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './NavBar.module.scss';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+
+interface NavBarProps {
+	className?: string;
+}
+
+export const NavBar = ({className}: NavBarProps) => {
+	return (
+		<div className={classNames(cls.Navbar, {}, [className])}>
+			<div className={cls.links}>
+				<AppLink theme={AppLinkTheme.SECONDARY} to='/' className={cls.mainLink}>
+					Главная
+				</AppLink>
+				<AppLink to={'/about'}>
+					О сайте
+				</AppLink> 
+			</div>
+		</div>
+	)
+}
+ 
